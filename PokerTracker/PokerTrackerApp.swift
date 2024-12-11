eb5217542ff3fddcd33043ba111dd020503849bc
Gootery <gautamrampur123@gmail.com>
2024-12-10 18:45:46 -0800

    Initial Commit

---
+++ PokerTracker/PokerTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerTrackerApp.swift
+//  PokerTracker
+//
+//  Created by Gautam Rampur on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PokerTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
